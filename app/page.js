5c906991d4d599c7e299530522ebff2c5047b0ba
Your Name <you@example.com>
2024-10-23 19:43:46 +0530

    Modify App

--- app/page.js
+++ app/page.js
@@ -3,6 +3,7 @@ import React from "react";
 import { BackgroundBeams } from "../components/ui/background-beams";
 import Navbar from "../components/Navbar";
 import Hero from "../components/Hero";
+import PS5Games from "../components/PS5Games";
 
 const Home = () => {
   return (
@@ -10,7 +11,7 @@ const Home = () => {
       <div className="main">
         <div className="gradient" />
         {/* <BackgroundBeams /> */}
-        <div className="relative z-10 flex justify-center items-center flex-col max-w-7xl mx-auto sm:px-16 px-6">
+        <div className="relative z-10 flex justify-center items-center flex-col max-w-7xl mx-auto sm:px-16 px-6 md:mt-30">
           <Navbar />
           <Hero />
         </div>
